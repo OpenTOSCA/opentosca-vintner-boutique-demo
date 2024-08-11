5e0f698a44109add043279678a3102952904ce31
Miles Stötzner <miles@stoetzner.de>
2024-08-11 23:11:37 +0200

    minor

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -19,26 +19,50 @@ if [ -z "${REGISTRY}" ]; then
   exit 1
 fi
 
-# Checkout
-docker build -t ${REGISTRY}/boutique-checkout:${TAG} src/checkout
-docker push ${REGISTRY}/boutique-checkout:${TAG}
+# Advertisement
+docker build -t ${REGISTRY}/boutique-advertisement:${TAG} src/advertisement
+docker push ${REGISTRY}/boutique-advertisement:${TAG}
+
+# Analytics
+docker build -t ${REGISTRY}/boutique-analytics:${TAG} src/analytical
+docker push ${REGISTRY}/boutique-analytics:${TAG}
 
 # Cart
 docker build -t ${REGISTRY}/boutique-cart:${TAG} src/cart/src
 docker push ${REGISTRY}/boutique-cart:${TAG}
 
-# Payment
-docker build -t ${REGISTRY}/boutique-payment:${TAG} src/payment
-docker push ${REGISTRY}/boutique-payment:${TAG}
+# Checkout
+docker build -t ${REGISTRY}/boutique-checkout:${TAG} src/checkout
+docker push ${REGISTRY}/boutique-checkout:${TAG}
+
+# Currency
+docker build -t ${REGISTRY}/boutique-currency:${TAG} src/currency
+docker push ${REGISTRY}/boutique-currency:${TAG}
+
+# Email
+docker build -t ${REGISTRY}/boutique-email:${TAG} src/email
+docker push ${REGISTRY}/boutique-email:${TAG}
 
 # Frontend
 docker build -t ${REGISTRY}/boutique-frontend:${TAG} src/frontend
 docker push ${REGISTRY}/boutique-frontend:${TAG}
 
+# Load Generator
+docker build -t ${REGISTRY}/boutique-loadgenerator:${TAG} src/loadgenerator
+docker push ${REGISTRY}/boutique-loadgenerator:${TAG}
+
+# Payment
+docker build -t ${REGISTRY}/boutique-payment:${TAG} src/payment
+docker push ${REGISTRY}/boutique-payment:${TAG}
+
+# Product
+docker build -t ${REGISTRY}/boutique-product:${TAG} src/product
+docker push ${REGISTRY}/boutique-product:${TAG}
+
 # Recommendation
 docker build -t ${REGISTRY}/boutique-recommendation:${TAG} src/recommendation
 docker push ${REGISTRY}/boutique-recommendation:${TAG}
 
-# Analytics
-docker build -t ${REGISTRY}/boutique-analytics:${TAG} src/analytical
-docker push ${REGISTRY}/boutique-analytics:${TAG}
+# Shipping
+docker build -t ${REGISTRY}/boutique-shipping:${TAG} src/shipping
+docker push ${REGISTRY}/boutique-shipping:${TAG}
